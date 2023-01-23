bdb3514a23cc6b26d08baf5495bfb8a21bdd63ff
Nuriddin321 <donkoko1996@gmail.com>
2023-01-23 21:55:41 +0500

    Entities implemented

--- src/ECommerce.Api.Product/Entities/Category.cs
+++ src/ECommerce.Api.Product/Entities/Category.cs
@@ -1,6 +1,12 @@
-﻿namespace ECommerce.Api.Product.Entities
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace ECommerce.Api.Product.Entities;
+
+public class Category
 {
-    public class Product
-    {
-    }
+    [BsonId]
+    public ObjectId Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
 }
--- src/ECommerce.Api.Product/Entities/Product.cs
+++ src/ECommerce.Api.Product/Entities/Product.cs
@@ -1,6 +1,17 @@
-﻿namespace ECommerce.Api.Product.Entities
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ECommerce.Api.Product.Entities;
+
+public class Product
 {
-    public class Product
-    {
-    }
+    [BsonId]
+    public ObjectId Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public string? ImageUrl { get; set; }
+    public Category? Category { get; set; }
+    public Brand? Brand { get; set; }
+    public int Count { get; set; }
 }
