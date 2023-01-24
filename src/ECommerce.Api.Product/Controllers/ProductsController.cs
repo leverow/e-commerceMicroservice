df16edd1a4f6131c5619ad553b8e5d3e86cb388d
Nuriddin321 <donkoko1996@gmail.com>
2023-01-24 13:17:28 +0500

    created Dto classes

---
+++ src/ECommerce.Api.Product/Controllers/ProductsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Api.Product.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+    }
+}
---
+++ src/ECommerce.Api.Product/Dtos/CategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerce.Api.Product.Dtos
+{
+    public class CategoryDto
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
