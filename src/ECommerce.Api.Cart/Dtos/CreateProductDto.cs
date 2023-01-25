3a9b0613487219f985842d825b3a77d0d8013eac
A3S4ROBOY <110540334+Akmalov-Asror@users.noreply.github.com>
2023-01-26 02:37:24 +0500

    new

---
+++ src/ECommerce.Api.Cart/Dtos/CreateProductDto.cs
@@ -0,0 +1,7 @@
+﻿namespace ECommerce.Api.Cart.Dtos;
+
+public class CreateProductDto
+{
+    public string? Id { get; set; }
+    public uint? Count { get; set; }
+}
--- src/ECommerce.Api.Cart/Entities/Cart.cs
+++ src/ECommerce.Api.Cart/Entities/Cart.cs
@@ -2,7 +2,7 @@
 
 public class Cart
 {
-    public int Id { get; set; }
+    public string? Id { get; set; }
     public int ProductId { get; set; }
     public uint Count { get; set; }
     public string? Properties { get; set; }
